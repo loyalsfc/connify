eac34390a116df726eb16002f119ff6c287cf125
Olumide Bambe <loyalsfc@gmail.com>
2023-06-02 01:37:30 +0100

    Fix deploy error

--- src/app/coins/[slug]/page.jsx
+++ src/app/coins/[slug]/page.jsx
@@ -1,11 +1,9 @@
-'use client'
-
 import React from 'react'
 import { Metadata } from 'next'
 // import useSWR from 'swr'
 import { fetcher, numberToString, toTwoDecimalPlace } from '../../../../utils/utils'
 import Image from 'next/image'
-import { FaAngleDown } from 'react-icons/fa'
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa'
 import axios from 'axios'
 
 export const metadata = {
@@ -35,11 +33,11 @@ async function getData(slug){
 }
 
 async function Page({params}) {
-    console.log(params)
     const data = await getData(params.slug)
     console.log(data)
 
-    const {logo} = data?.metadata
+    const {logo, name, symbol} = data?.metadata
+    const {quote} = data?.coinData
 
     return (
         <main className='py-6'>
@@ -52,15 +50,15 @@ async function Page({params}) {
                             height={32}
                             alt="Coin logo"
                         />
-                        <h1 className='text-3xl'>{data?.coinData?.name}</h1>
-                        <span className='text-sm py-0.5 px-1.5 bg-faded-grey text-medium-grey rounded'>{data?.coinData?.symbol}</span>
+                        <h1 className='text-3xl'>{name}</h1>
+                        <span className='text-sm py-0.5 px-1.5 bg-faded-grey text-medium-grey rounded'>{symbol}</span>
                     </div> 
                     <div className='flex gap-4 items-center'>
-                        <h2 className='font-bold text-3xl text-black'>${numberToString(data?.coinData?.quote.USD.price)}</h2>
+                        <h2 className='font-bold text-3xl text-black'>${numberToString(quote.USD.price)}</h2>
                         <span 
-                            className='border-md bg-red-500 text-white py-[5px] flex items-center font-medium gap-1 rounded-md px-2.5 text-sm'
+                            className={`border-md ${quote.USD.percent_change_24h > 0 ? "bg-green-500" :'bg-red-500'} text-white py-[5px] flex items-center font-medium gap-1 rounded-md px-2.5 text-sm'`}
                         >
-                            <FaAngleDown/> {toTwoDecimalPlace(data?.coinData?.quote.USD.percent_change_24h)}% 
+                            {quote.USD.percent_change_24h > 0 ? <FaAngleUp /> : <FaAngleDown/>} {toTwoDecimalPlace(quote.USD.percent_change_24h)}% 
                         </span>
                     </div>
                 </article>
